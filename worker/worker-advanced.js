064f522b2bc6f2141fcfda64d859bfd26d41a8d4
Faehan Isteak Taseen <55633040+Tas33n@users.noreply.github.com>
2021-12-22 22:29:18 +0600

    Update worker-advanced.js

--- worker/worker-advanced.js
+++ worker/worker-advanced.js
@@ -102,7 +102,7 @@ const uiConfig = {
     "downloaddomain": domain_for_dl, // Ignore this and set domains at top of this page after service accounts.
     "poster": "https://cdn.jsdelivr.net/npm/@googledrive/index@2.0.20/images/poster.jpg", // Video poster URL or see Readme to how to load from Drive
     "audioposter": "https://cdn.jsdelivr.net/npm/@googledrive/index@2.0.20/images/music.jpg", // Video poster URL or see Readme to how to load from Drive
-    "jsdelivr_cdn_src": "https://cdn.jsdelivr.net/npm/@googledrive/index", // If Project is Forked, then enter your GitHub repo
+    "jsdelivr_cdn_src": "https://cdn.jsdelivr.net/gh/tas33n/tasu-index@V2.0.26/", // If Project is Forked, then enter your GitHub repo
     "render_head_md": true, // Render Head.md
     "render_readme_md": true, // Render Readme.md
     "display_drive_link": false, // This will add a Link Button to Google Drive of that particular file.
@@ -530,7 +530,7 @@ async function handleRequest(request) {
     if (path.toLowerCase() == '/arc-sw.js') {
         return fetch("https://arc.io/arc-sw.js")
     } else if (path.toLowerCase() == '/admin') {
-        return Response.redirect("https://www.npmjs.com/package/@googledrive/index", 301)
+        return Response.redirect("https://cdn.jsdelivr.net/gh/tas33n/tasu-index@V2.0.26/", 301)
     } else if (blocked_region.includes(region)) {
         return new Response(asn_blocked, {
             status: 403,
